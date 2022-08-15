7342baba083bbdc2726b5e4293f2ad6fd9b29c3f
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-15 16:33:13 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+import './src/styles/style.css';
+import './src/index.css';
+import { index } from './src/index';
+
+document.querySelector('#app').innerHTML = index();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export const index = () => {
+    return `
+        <h1 class="text-tahiti-500">Hello World!!!</h1>
+    `;
+}
