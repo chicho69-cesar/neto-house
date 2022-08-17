3a1e07cd22771f06f5954adaf82d8da35f45c230
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-08-16 23:13:29 -0500

    End show details for the houseworks

---
+++ src/modules/dates.js
@@ -0,0 +1,59 @@
+export const kitchenDates = () => {
+    return [
+        {
+            member: 'Luis Ernesto',
+            day: '25/Julio/2022',
+            limitDay: '26/Julio/2022'
+        },
+        {
+            member: 'Gabriel',
+            day: '27/Julio/2022',
+            limitDay: '28/Julio/2022'
+        },
+        {
+            member: 'Diego',
+            day: '29/Julio/2022',
+            limitDay: '30/Julio/2022'
+        },
+        {
+            member: 'Francisco',
+            day: '01/Agosto/2022',
+            limitDay: '02/Agosto/2022'
+        },
+        {
+            member: 'Brian',
+            day: '03/Agosto/2022',
+            limitDay: '04/Agosto/2022'
+        },
+        {
+            member: 'Oliver',
+            day: '05/Agosto/2022',
+            limitDay: '06/Agosto/2022'
+        },
+        {
+            member: 'Jose',
+            day: '08/Agosto/2022',
+            limitDay: '09/Agosto/2022'
+        },
+        {
+            member: 'Andres',
+            day: '10/Agosto/2022',
+            limitDay: '11/Agosto/2022'
+        },
+        {
+            member: 'Adrian',
+            day: '12/Agosto/2022',
+            limitDay: '13/Agosto/2022'
+        },
+        {
+            member: 'Cesar',
+            day: '16/Agosto/2022',
+            limitDay: '17/Agosto/2022'
+        },
+        {
+            member: 'Samuel',
+            day: '18/Agosto/2022',
+            limitDay: '19/Agosto/2022'
+        }
+    ];
+}
--- src/modules/load-info.js
+++ src/modules/load-info.js
@@ -1,6 +1,21 @@
+import kitchenTurns from "../components/kitchenTurns";
+import { kitchenDates } from "./dates";
+
 const loadoInfo = element => {
-    console.log(element);
-    console.log(element.classList[0]);
+    let consultType = +element.classList[0];
+    const mainContainer = document.querySelector('main');
+
+    if (consultType === 1) {
+        mainContainer.innerHTML = kitchenTurns(kitchenDates());
+    } else {
+        mainContainer.innerHTML = `
+            <section class="col-start-1 col-end-3 w-full text-center py-8">
+                <h1 class="text-3xl text-red-500">
+                    Aún no se que onda con los baños 😥🤙
+                </h1>
+            </section>
+        `;
+    }
 }
 
 export default loadoInfo;
