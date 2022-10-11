311d54acab7645adc7e79928767764aa02d2a655
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-10-10 23:35:06 -0500

    Correction in the october dates

--- src/modules/dates.js
+++ src/modules/dates.js
@@ -8,47 +8,47 @@ export const kitchenDates = () => {
         {
             member: 'Andy Jhonson',
             day: '30/Sept/2022',
-            limitDay: '01/Oct/2022'
+            limitDay: '01/Octubre/2022'
         },
         {
             member: 'Adrian Falacioso',
-            day: '03/Oct/2022',
-            limitDay: '04/Oct/2022'
+            day: '03/Octubre/2022',
+            limitDay: '04/Octubre/2022'
         },
         {
             member: 'Cesar Me Duermo',
-            day: '05/Oct/2022',
-            limitDay: '06/Oct/2022'
+            day: '05/Octubre/2022',
+            limitDay: '06/Octubre/2022'
         },
         {
             member: 'Sammy Orines',
-            day: '07/Oct/2022',
-            limitDay: '08/Oct/2022'
+            day: '07/Octubre/2022',
+            limitDay: '08/Octubre/2022'
         },
         {
             member: 'Luis Alcoholico',
-            day: '10/Oct/2022',
-            limitDay: '11/Oct/2022'
+            day: '10/Octubre/2022',
+            limitDay: '11/Octubre/2022'
         },
         {
             member: 'Gabby Jueves',
-            day: '12/Oct/2022',
-            limitDay: '13/Oct/2022'
+            day: '12/Octubre/2022',
+            limitDay: '13/Octubre/2022'
         },
         {
             member: 'Diego Adrian Verde',
-            day: '14/Oct/2022',
-            limitDay: '15/Oct/2022'
+            day: '14/Octubre/2022',
+            limitDay: '15/Octubre/2022'
         },
         {
             member: 'Brian Atxidentes',
-            day: '17/Oct/2022',
-            limitDay: '18/Oct/2022'
+            day: '17/Octubre/2022',
+            limitDay: '18/Octubre/2022'
         },
         {
             member: 'Hermilio Onta la botella',
-            day: '19/Oct/2022',
-            limitDay: '20/Oct/2022'
+            day: '19/Octubre/2022',
+            limitDay: '20/Octubre/2022'
         }
     ];
 }
