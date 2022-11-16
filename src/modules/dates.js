0ee6eb50769278eb6f7e56b6658cf7cecb1b4ef5
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-11-15 21:29:16 -0600

    Dates of second part of november

--- src/modules/dates.js
+++ src/modules/dates.js
@@ -2,53 +2,53 @@ export const kitchenDates = () => {
     return [
         {
             member: 'Jose Chico Fit',
-            day: '21/Octubre/2022',
-            limitDay: '22/Octubre/2022'
+            day: '14/Nov/2022',
+            limitDay: '15/Nov/2022'
         },
         {
             member: 'Andy Jhonson',
-            day: '24/Octubre/2022',
-            limitDay: '25/Octubre/2022'
+            day: '16/Nov/2022',
+            limitDay: '17/Nov/2022'
         },
         {
             member: 'Adrian Falacioso',
-            day: '26/Octubre/2022',
-            limitDay: '27/Octubre/2022'
+            day: '18/Nov/2022',
+            limitDay: '19/Nov/2022'
         },
         {
             member: 'Cesar Me Duermo',
-            day: '28/Octubre/2022',
-            limitDay: '29/Octubre/2022'
+            day: '21/Nov/2022',
+            limitDay: '22/Nov/2022'
         },
         {
             member: 'Sammy Orines',
-            day: '31/Octubre/2022',
-            limitDay: '01/Nov/2022'
+            day: '23/Nov/2022',
+            limitDay: '24/Nov/2022'
         },
         {
             member: 'Luis Alcoholico',
-            day: '02/Nov/2022',
-            limitDay: '03/Nov/2022'
+            day: '25/Nov/2022',
+            limitDay: '26/Nov/2022'
         },
         {
             member: 'Gabby Jueves',
-            day: '04/Nov/2022',
-            limitDay: '05/Nov/2022'
+            day: '28/Nov/2022',
+            limitDay: '29/Nov/2022'
         },
         {
             member: 'Diego Adrian Verde',
-            day: '07/Nov/2022',
-            limitDay: '08/Nov/2022'
+            day: '30/Nov/2022',
+            limitDay: '01/Dic/2022'
         },
         {
             member: 'Brian Atxidentes',
-            day: '09/Nov/2022',
-            limitDay: '10/Nov/2022'
+            day: '02/Dic/2022',
+            limitDay: '03/Dic/2022'
         },
         {
             member: 'Hermilio Onta la botella',
-            day: '11/Nov/2022',
-            limitDay: '12/Nov/2022'
+            day: '05/Dic/2022',
+            limitDay: '06/Dic/2022'
         }
     ];
 }
