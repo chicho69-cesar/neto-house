98424f73c4653c504243caa8bb5bc9411dfaf175
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-12-20 22:49:54 -0600

    Dates for the first part of December

--- src/modules/dates.js
+++ src/modules/dates.js
@@ -2,53 +2,43 @@ export const kitchenDates = () => {
     return [
         {
             member: 'Jose Chico Fit',
-            day: '14/Nov/2022',
-            limitDay: '15/Nov/2022'
+            day: '07/Dic/2022',
+            limitDay: '08/Dic/2022'
         },
         {
             member: 'Andy Jhonson',
-            day: '16/Nov/2022',
-            limitDay: '17/Nov/2022'
+            day: '09/Dic/2022',
+            limitDay: '10/Dic/2022'
         },
         {
             member: 'Adrian Falacioso',
-            day: '18/Nov/2022',
-            limitDay: '19/Nov/2022'
+            day: '12/Dic/2022',
+            limitDay: '13/Dic/2022'
         },
         {
             member: 'Cesar Me Duermo',
-            day: '21/Nov/2022',
-            limitDay: '22/Nov/2022'
+            day: '14/Dic/2022',
+            limitDay: '15/Dic/2022'
         },
         {
             member: 'Sammy Orines',
-            day: '23/Nov/2022',
-            limitDay: '24/Nov/2022'
+            day: '16/Dic/2022',
+            limitDay: '17/Dic/2022'
         },
         {
             member: 'Luis Alcoholico',
-            day: '25/Nov/2022',
-            limitDay: '26/Nov/2022'
+            day: '19/Dic/2022',
+            limitDay: '20/Dic/2022'
         },
         {
             member: 'Gabby Jueves',
-            day: '28/Nov/2022',
-            limitDay: '29/Nov/2022'
+            day: '21/Dic/2022',
+            limitDay: '22/Dic/2022'
         },
         {
             member: 'Diego Adrian Verde',
-            day: '30/Nov/2022',
-            limitDay: '01/Dic/2022'
-        },
-        {
-            member: 'Brian Atxidentes',
-            day: '02/Dic/2022',
-            limitDay: '03/Dic/2022'
-        },
-        {
-            member: 'Hermilio Onta la botella',
-            day: '05/Dic/2022',
-            limitDay: '06/Dic/2022'
+            day: '23/Dic/2022',
+            limitDay: '24/Dic/2022'
         }
     ];
 }
