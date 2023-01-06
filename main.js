2b3ce8ce4d827072e23f3e145d289008f4ea458c
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-05 23:17:39 -0600

    :construction: New desing is unavaliable

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 import './src/styles/style.css';
 import './src/index.css';
+
 import index from './src/index';
 import { message } from './src/modules/message';
-import loadoInfo from './src/modules/load-info';
 
 document.querySelector('#app').innerHTML = index();
 
 document.querySelector('#open-modal').addEventListener('click', () => {
-    message();
-});
-
-document.querySelectorAll('article').forEach(article => {
-    article.addEventListener('click', e => {
-        loadoInfo(e.target);
-    });
+  message();
 });
 
 document.body.onload = function() {
-    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    
-    if (userPrefersDark) {
-        document.documentElement.classList.replace('light', 'dark');
-    }
+	const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+	
+	if (userPrefersDark) {
+		document.documentElement.classList.replace('light', 'dark');
+	}
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import header from "./components/header";
-import container from "./components/container";
 import modal from "./components/modal";
 
 const index = () => {
-    return `
-        ${ header() } 
-        ${ container() } 
-        ${ modal() }
-    `;
+	return `
+		${ modal() }
+	`;
 }
 
 export default index;
