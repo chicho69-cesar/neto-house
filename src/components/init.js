3a008d34cce511d41fdaa81377951990fb75f745
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-06 21:39:28 -0600

    :sparkles: Add index section in home

--- src/components/init.js
+++ src/components/init.js
@@ -1,4 +1,4 @@
-export default function init() {
+const init = () => {
   return `
     <main>
       <section>
@@ -19,3 +19,5 @@ export default function init() {
     </main>
   `;
 }
+
+export default init;
