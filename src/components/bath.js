c79bb9c307e52627f012e0df5e7ef408dc80f058
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-13 14:40:35 -0600

    ✨ Add baths section

--- src/components/bath.js
+++ src/components/bath.js
@@ -1,5 +1,34 @@
+import { getBathTurn } from "../utils/baths";
+import bathComponent from "./bath-component";
+
 const bath = () => {
-  return ``;
+  return `
+    <div class="pt-12 my-20">
+      <div class="w-full md:w-2/3 mx-auto">
+        <h3 class="text-3xl font-bold text-black dark:text-white text-center mb-4">
+          ¿Rol de los baños?
+        </h3>
+
+        <p class="text-lg text-black dark:text-white text-center leading-6">
+          El aseo de los baños es responsabilidad de cada quien el baño 
+          que le corresponde, ahi cada quien sabe cuando le toca y que es 
+          lo que le toca hacer para limpiar su baño.
+        </p>
+      </div>
+
+      <section class="my-12 flex flex-row flex-wrap gap-4 md:gap-12 justify-center">
+        ${ bathComponent(getBathTurn(1), true) }
+      </section>
+
+      <section class="my-12 flex flex-row flex-wrap gap-4 md:gap-12 justify-center">
+        ${ bathComponent(getBathTurn(2), false) }
+      </section>
+      
+      <section class="my-12 flex flex-row flex-wrap gap-4 md:gap-12 justify-center">
+        ${ bathComponent(getBathTurn(3), true) }
+      </section>
+    </div>
+  `;
 }
 
 export default bath;
