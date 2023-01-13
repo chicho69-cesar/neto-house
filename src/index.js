88f47610e5ede146e83d85573183f98f1c3ef360
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-01-13 12:09:36 -0600

    🎨 Improve the format of the code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import bath from "./components/bath";
+import buttonModal from "./components/button-modal";
 import header from "./components/header";
 import init from "./components/init";
+import kitchen from "./components/kitchen";
 import modal from "./components/modal";
 
 const index = () => {
@@ -7,7 +10,10 @@ const index = () => {
 		<div class="w-4/5 mx-auto p-6">
 			${ header() }
 			${ init() }
+			${ bath() }
+			${ kitchen() }
 
+			${ buttonModal() }
 			${ modal() }
 		</div>
 	`;
--- src/utils/roomies.js
+++ src/utils/roomies.js
@@ -2,7 +2,7 @@ import { kitchenDates, roomies } from "../modules/data";
 import { getCurrentDate } from "./dates";
 
 export function getRoomieTurn() {
-  let { day, month } = getCurrentDate()
+  let { day, month } = getCurrentDate();
   let dates = kitchenDates();
 
   let dataCleanliness = dates.find(date => date.month === month);
@@ -17,12 +17,35 @@ export function getRoomieTurn() {
     };
   }
   
-  let roomie = roomies().find(room => room.id === roomieDataCleanliness.roomie)
+  let roomie = roomies().find(room => room.id === roomieDataCleanliness.roomie);
 
   return {
     id: roomie.id,
     title: 'El aseo de hoy le toca a',
     person: roomie.name,
     band: true
+  };
+}
+
+export function getRoomieTodayTurn() {
+  let { day, month } = getCurrentDate();
+  let dates = kitchenDates();
+  let roomiesInfo = roomies();
+
+  let dataCleanliness = dates.find(date => date.month === month);
+  let roomieDataCleanliness = dataCleanliness.dates.find(date => date.day === day || date.limit === day);
+  
+  if (roomieDataCleanliness === undefined) {
+    return {
+      todayFree: true,
+      roomie: {}
+    };
+  }
+
+  let roomie = roomiesInfo.find(room => room.id === roomieDataCleanliness.roomie)
+
+  return {
+    todayFree: false,
+    roomie
   };
 }
