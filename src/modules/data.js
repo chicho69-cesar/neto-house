e400040ea0b3dd4391cbbc1599797d501563bcec
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-25 10:08:36 -0600

    :sparkles: Update new dates, names and page styles

--- src/modules/data.js
+++ src/modules/data.js
@@ -2,49 +2,49 @@ export function roomies() {
 	return [
 		{
 			id: 1,
-			name: 'Jose Chicho Fit',
+			name: 'Jose',
 			image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
 			color: 'red'
 		},
 		{
 			id: 2,
-			name: 'Andy Jhonson',
+			name: 'Andres',
 			image: 'https://i.pinimg.com/564x/34/f9/07/34f9072d158d176d922d652975982a18.jpg',
 			color: 'green'
 		},
 		{
 			id: 3,
-			name: 'Adrian Falacioso',
+			name: 'Adrian',
 			image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
 			color: 'blue'
 		},
 		{
 			id: 4,
-			name: 'Cesar Me Duermo',
+			name: 'Cesar',
 			image: 'https://i.pinimg.com/736x/2c/4c/67/2c4c67f144c8ed1600be38d06d8d1765.jpg',
 			color: 'purple'
 		},
 		{
 			id: 5,
-			name: 'Sammy Orines',
+			name: 'Samuel',
 			image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
 			color: 'Brown'
 		},
 		{
 			id: 6,
-			name: 'Luis Alcoholico',
+			name: 'Luis Ernesto',
 			image: 'https://i.pinimg.com/564x/44/bd/b5/44bdb5ea56d5309b9c4c73ec13e45035.jpg',
 			color: 'SteelBlue'
 		},
 		{
 			id: 7,
-			name: 'Gabby Jueves',
+			name: 'Gabriel',
 			image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
 			color: 'Maroon'
 		},
 		{
 			id: 8,
-			name: 'Diegoooooo',
+			name: 'Diego',
 			image: 'https://i.pinimg.com/564x/bd/9e/03/bd9e03f51010761ddd33bc866f0bc8e4.jpg',
 			color: 'DarkTurquoise'
 		},
@@ -151,34 +151,17 @@ export function kitchenDates() {
 			month: 0,
 			dates: [
 				{ day: 12, limit: 13, roomie: 4 },
-				{ day: 23, limit: 24, roomie: 1 },
-				{ day: 25, limit: 26, roomie: 2 },
-				{ day: 27, limit: 28, roomie: 3 },
-				{ day: 30, limit: 31, roomie: 4 },
 			]
 		},
 		{
 			month: 1,
 			dates: [
 				{ day: 1, limit: 2, roomie: 5 },
-				{ day: 3, limit: 4, roomie: 6 },
-				{ day: 6, limit: 7, roomie: 7 },
-				{ day: 8, limit: 9, roomie: 8 },
-				{ day: 10, limit: 11, roomie: 9 },
-				{ day: 13, limit: 14, roomie: 1 },
-				{ day: 15, limit: 16, roomie: 2 },
-				{ day: 17, limit: 18, roomie: 3 },
-				{ day: 20, limit: 21, roomie: 4 },
-				{ day: 22, limit: 23, roomie: 5 },
-				{ day: 24, limit: 25, roomie: 6 },
-				{ day: 27, limit: 28, roomie: 7 },
 			]
 		},
 		{
 			month: 2,
 			dates: [
-				{ day: 1, limit: 2, roomie: 8 },
-				{ day: 3, limit: 4, roomie: 9 },
 				{ day: 6, limit: 7, roomie: 1 },
 				{ day: 8, limit: 9, roomie: 2 },
 				{ day: 10, limit: 11, roomie: 3 },
@@ -250,37 +233,94 @@ export function kitchenDates() {
 		{
 			month: 6,
 			dates: [
-				{ day: 3, limit: 4, roomie: 7 },
+				{ day: 31, limit: 1, roomie: 1 },
 			]
 		},
 		{
 			month: 7,
 			dates: [
-				{ day: 2, limit: 3, roomie: 8 },
+				{ day: 2, limit: 3, roomie: 2 },
+				{ day: 4, limit: 5, roomie: 3 },
+				{ day: 7, limit: 8, roomie: 4 },
+				{ day: 9, limit: 10, roomie: 5 },
+				{ day: 11, limit: 12, roomie: 6 },
+				{ day: 14, limit: 15, roomie: 7 },
+				{ day: 16, limit: 17, roomie: 8 },
+				{ day: 18, limit: 19, roomie: 9 },
+				{ day: 21, limit: 22, roomie: 1 },
+				{ day: 23, limit: 24, roomie: 2 },
+				{ day: 25, limit: 26, roomie: 3 },
+				{ day: 28, limit: 29, roomie: 4 },
+				{ day: 30, limit: 31, roomie: 5 },
 			]
 		},
 		{
 			month: 8,
 			dates: [
-				{ day: 1, limit: 2, roomie: 9 },
+				{ day: 1, limit: 2, roomie: 6 },
+				{ day: 4, limit: 5, roomie: 7 },
+				{ day: 6, limit: 7, roomie: 8 },
+				{ day: 8, limit: 9, roomie: 9 },
+				{ day: 11, limit: 12, roomie: 1 },
+				{ day: 13, limit: 14, roomie: 2 },
+				{ day: 15, limit: 16, roomie: 3 },
+				{ day: 18, limit: 19, roomie: 4 },
+				{ day: 20, limit: 21, roomie: 5 },
+				{ day: 22, limit: 23, roomie: 6 },
+				{ day: 25, limit: 26, roomie: 7 },
+				{ day: 27, limit: 28, roomie: 8 },
+				{ day: 29, limit: 30, roomie: 9 },
 			]
 		},
 		{
 			month: 9,
 			dates: [
 				{ day: 2, limit: 3, roomie: 1 },
+				{ day: 4, limit: 5, roomie: 2 },
+				{ day: 6, limit: 7, roomie: 3 },
+				{ day: 9, limit: 10, roomie: 4 },
+				{ day: 11, limit: 12, roomie: 5 },
+				{ day: 13, limit: 14, roomie: 6 },
+				{ day: 16, limit: 17, roomie: 7 },
+				{ day: 18, limit: 19, roomie: 8 },
+				{ day: 20, limit: 21, roomie: 9 },
+				{ day: 23, limit: 24, roomie: 1 },
+				{ day: 25, limit: 26, roomie: 2 },
+				{ day: 27, limit: 28, roomie: 3 },
+				{ day: 30, limit: 31, roomie: 4 },
 			]
 		},
 		{
 			month: 10,
 			dates: [
-				{ day: 1, limit: 2, roomie: 2 },
+				{ day: 1, limit: 2, roomie: 5 },
+				{ day: 3, limit: 4, roomie: 6 },
+				{ day: 6, limit: 7, roomie: 7 },
+				{ day: 8, limit: 9, roomie: 8 },
+				{ day: 10, limit: 11, roomie: 9 },
+				{ day: 13, limit: 14, roomie: 1 },
+				{ day: 15, limit: 16, roomie: 2 },
+				{ day: 17, limit: 18, roomie: 3 },
+				{ day: 20, limit: 21, roomie: 4 },
+				{ day: 22, limit: 23, roomie: 5 },
+				{ day: 24, limit: 25, roomie: 6 },
+				{ day: 27, limit: 28, roomie: 7 },
+				{ day: 29, limit: 30, roomie: 8 },
 			]
 		},
 		{
 			month: 11,
 			dates: [
-				{ day: 1, limit: 2, roomie: 3 },
+				{ day: 1, limit: 2, roomie: 9 },
+				{ day: 4, limit: 5, roomie: 1 },
+				{ day: 6, limit: 7, roomie: 2 },
+				{ day: 8, limit: 9, roomie: 3 },
+				{ day: 11, limit: 12, roomie: 4 },
+				{ day: 13, limit: 14, roomie: 5 },
+				{ day: 15, limit: 16, roomie: 6 },
+				{ day: 18, limit: 19, roomie: 7 },
+				{ day: 20, limit: 21, roomie: 8 },
+				{ day: 22, limit: 23, roomie: 9 },
 			]
 		},
 	];
