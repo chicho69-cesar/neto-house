56ce265150d199cb1a26f5909921114ad26ad0a3
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-06 21:19:18 -0600

    :fire: Remove old only js files components

--- src/components/kitchen.js
+++
@@ -1,9 +0,0 @@
-const kitchen = () => {
-  return `
-    <div id="kitchen">
-      <div id="calendar" class="my-16 pt-6"></div>
-    </div>
-  `;
-}
-
-export default kitchen;
--- src/modules/turn.js
+++
@@ -1,5 +0,0 @@
-import { getRoomieTurn } from "../utils/roomies";
-
-export default function getInfo() {
-	return getRoomieTurn();
-}
